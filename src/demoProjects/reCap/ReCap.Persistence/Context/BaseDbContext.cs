9f9991faaf5a7ad9da6c7b7175e2c83ea40f8762
berkayhellagun <berkayhellagun@hotmail.com>
2022-08-17 10:46:51 +0300

    add repository configuration

---
+++ src/demoProjects/reCap/ReCap.Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ReCap.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCap.Persistence.Context
+{
+    public class BaseDbContext : DbContext
+    {
+        IConfiguration _configuration;
+        public DbSet<Brand> Brands { get; set; }
+
+        public BaseDbContext(IConfiguration configuration, DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(b =>
+            {
+                //create table
+                b.ToTable("Brands").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            Brand[] brandSeed = { new(1, "Test"), new(2, "Test") };
+            modelBuilder.Entity<Brand>().HasData(brandSeed);
+        }
+    }
+}
---
+++ src/demoProjects/reCap/ReCap.Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+using ReCap.Application.Services.Repositories;
+using ReCap.Domain.Entities;
+using ReCap.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCap.Persistence.Repositories
+{
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext baseDbContext) : base(baseDbContext)
+        {
+
+        }
+    }
+}
