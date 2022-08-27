a5feb4ea7dd1a78906246395d1d989468c53148e
berkayhellagun <berkayhellagun@hotmail.com>
2022-08-27 15:01:08 +0300

    add services

--- src/demoProjects/reCap/WebAPI/Program.cs
+++ src/demoProjects/reCap/WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using ReCap.Application;
+using ReCap.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceService(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
