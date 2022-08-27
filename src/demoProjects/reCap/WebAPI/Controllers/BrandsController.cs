401ae2e983209e47491db8026668e1ae00e3c078
berkayhellagun <berkayhellagun@hotmail.com>
2022-08-27 15:01:41 +0300

    create controller

---
+++ src/demoProjects/reCap/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReCap.Application.Features.Brands.Commands.CreateBrand;
+using ReCap.Application.Features.Brands.Dtos;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            var result = await Mediator.Send(createBrandCommand);
+            return Created("Created", result);
+        }
+    }
+}
