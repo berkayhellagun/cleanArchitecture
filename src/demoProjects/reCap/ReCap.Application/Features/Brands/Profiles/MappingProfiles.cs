65c21ba01808c6979552aae5efb4322a07c76653
berkayhellagun <berkayhellagun@hotmail.com>
2022-09-03 09:26:44 +0300

    add list query of brand

--- src/demoProjects/reCap/ReCap.Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/demoProjects/reCap/ReCap.Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Core.Persistence.Paging;
 using ReCap.Application.Features.Brands.Commands.CreateBrand;
 using ReCap.Application.Features.Brands.Dtos;
+using ReCap.Application.Features.Brands.Models;
 using ReCap.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -18,6 +20,7 @@ namespace ReCap.Application.Features.Brands.Profiles
             CreateMap<Brand, CreatedBrandDto>().ReverseMap();
             // Command hold fields so we can use this map rules
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
         }
     }
 }
---
+++ src/demoProjects/reCap/ReCap.Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using MediatR;
+using ReCap.Application.Features.Brands.Models;
+using ReCap.Application.Services.Repositories;
+using ReCap.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCap.Application.Features.Brands.Queries.GetListBrand
+{
+    public class GetListBrandQuery : IRequest<BrandListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListModel>
+        {
+            readonly IBrandRepository _brandRepository;
+            readonly IMapper _mapper;
+
+            public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<BrandListModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Brand> brands = await _brandRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+                BrandListModel brandListModel = _mapper.Map<BrandListModel>(brands);
+                return brandListModel;
+            }
+        }
+    }
+}
